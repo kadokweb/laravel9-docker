0f4199dedcded2bf19df68273408776e1bb4f5b6
kadokwed <kadokwr@gmail.com>
2022-06-06 03:41:04 -0300

    Add tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') Laravel 9 Docker</title>
-
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
